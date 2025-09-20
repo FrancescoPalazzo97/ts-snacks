2dec0de883e877bb1908d4e9ba13b8fdc226992c
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-09-20 16:47:26 +0200

    UPD: testing advanced schema

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,45 @@
-import { z } from 'zod';
+import { date, z } from 'zod';
 z.config(z.locales.it());
 
-const Product = z.object({
-  id: z.number(),
-  name: z.string(),
-  price: z.number().positive(),
-  available: z.boolean(),
-  category: z.array(z.string())
-})
+// const Product = z.object({
+//   id: z.number().int(),
+//   name: z.string(),
+//   price: z.number().positive(),
+//   available: z.boolean(),
+//   category: z.array(z.string())
+// })
 
-type Product = z.infer<typeof Product>;
+// type Product = z.infer<typeof Product>;
 
-const product1 = {
-  id: 1,
-  name: 'Mela',
-  price: 2,
-  available: true,
-  category: ['fruit', 'red']
-}
+// const product1 = {
+//   id: 1,
+//   name: 'Mela',
+//   price: 2,
+//   available: true,
+//   category: ['fruit', 'red']
+// }
 
-const product2 = {
-  id: 1,
-  name: 'banana',
-  price: 2,
-  available: false,
-  category: ['fruit', 'yellow']
-}
+// const product2 = {
+//   id: 1,
+//   name: 'banana',
+//   price: 2,
+//   available: false,
+//   category: ['fruit', 'yellow']
+// }
 
-const product3 = {
-  id: 1,
-  name: 'fragola',
-  price: 2,
-  available: 'maybe',
-  category: 'red'
-}
+// const product3 = {
+//   id: 1,
+//   name: 'fragola',
+//   price: 2,
+//   available: 'maybe',
+//   category: 'red'
+// }
 
-const data = Product.parse(product1);
-console.log(data);
+// const data = Product.parse(product1);
+// console.log(data);
 
-const data2 = Product.parse(product2);
-console.log(data2);
+// const data2 = Product.parse(product2);
+// console.log(data2);
 
 // try {
 //   const data3 = Product.parse(product3);
@@ -52,5 +52,44 @@ console.log(data2);
 //   }
 // }
 
-const data3 = Product.safeParse(product3);
-console.log(data3)
+// const data3 = Product.safeParse(product3);
+// console.log(data3)
+
+const ProdottoAvanzatoSchema = z.object({
+  id: z.number().int().positive(),
+  name: z.string().min(3).max(100), // lunghezza tra 3 e 100 caratteri
+  price: z.number().positive().multipleOf(0.01), // precisione centesimi (no millesimi)
+  available: z.boolean(),
+  category: z.array(z.string()).min(1).max(5), // almeno 1, massimo 5 categorie
+  bio: z.string().optional(), // campo opzionale
+  discount: z.number().min(0).max(100).nullable(), // percentuale sconto, può essere null
+  createdAt: z.iso.datetime() // ISO datetime
+});
+
+type ProductType = z.infer<typeof ProdottoAvanzatoSchema>;
+
+const p1 = {
+  id: 1,
+  name: 'Pane',
+  price: 1.50,
+  available: true,
+  category: ['Cibo', 'pane'],
+  discount: null,
+  createdAt: new Date().toISOString()
+}
+
+const data = ProdottoAvanzatoSchema.parse(p1);
+console.log(data)
+
+const p2 = {
+  id: undefined,
+  name: null,
+  price: true,
+  available: true,
+  category: 'cibo',
+  discount: 120,
+  createdAt: Date()
+}
+
+const data2 = ProdottoAvanzatoSchema.parse(p2);
+console.log(data2)
